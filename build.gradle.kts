8daebfb5ee8baf68db31359bc09264b4cc01899f
Your Name <you@example.com>
2024-10-28 09:33:11 +0100

    add jar generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.34")
     testImplementation("org.spockframework:spock-core:2.4-M4-groovy-4.0")
 }
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "org.football.Main"
+}
